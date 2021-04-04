07383f48b6b2edf38c56b7de8c039d933e3a09f7
ThomasPoultney <tompoultney@btinternet.com>
2021-04-04 20:59:05 +0100

    Added Tree and Node Class
    Added Tree and Node Class for minimax algorithm

---
+++ Classes/Data Structures/Node.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessB
+{
+    class Node
+    {
+        private Board board;
+        private Node parent;
+        private List<Node> children = new List<Node>();
+    }
+}
